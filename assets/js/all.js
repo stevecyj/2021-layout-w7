639d1a7445d899ed4214979068a100de0a5084e2
root <root@localhost.localdomain>
2021-09-05 20:30:06 +0800

    Update 2021-09-05T12:29:57.091Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,7 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+console.log('Hello!');
+$(document).ready(function () {
+  console.log('HesSchool Hello!');
 });
 //# sourceMappingURL=all.js.map
